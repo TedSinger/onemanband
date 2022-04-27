ccefb04b2bcf2fd762a8988fd8b03dfde60f5734
Ted Singer <tedsinger1@gmail.com>
2022-04-26 20:54:19 -0400

    modular Caddyfiles

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -e "ssh -i '/home/ted/Documents/secrets/blurbcloud_rsa'" -rA . root@blurb.cloud:/home/blurb/priory
+rsync -e "ssh -i '/home/ted/Documents/secrets/blurbcloud_rsa'" -rA --delete . root@blurb.cloud:/home/blurb/priory
 ssh -i '/home/ted/Documents/secrets/blurbcloud_rsa' root@blurb.cloud 'sudo -u blurb CWD=/home/blurb/priory /home/blurb/priory/install-services.sh'
