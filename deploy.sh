be52f440068debc446c614f4b006c186829412e9
Ted Singer <tedsinger1@gmail.com>
2022-06-01 22:32:42 -0400

    actually restart services

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 KEYFILE=/home/ted/Documents/secrets/blurbcloud_rsa
 REMOTE_PRIORY_ROOT=/home/priory
 rsync -e "ssh -i '$KEYFILE'" -rA --delete . root@blurb.cloud:$REMOTE_PRIORY_ROOT
-ssh -i $KEYFILE root@blurb.cloud "export CWD=$REMOTE_PRIORY_ROOT && cd $REMOTE_PRIORY_ROOT && $REMOTE_PRIORY_ROOT/create-users.sh && $REMOTE_PRIORY_ROOT/install-toolchain.sh && $REMOTE_PRIORY_ROOT/install-all-services.sh"
+ssh -i $KEYFILE root@blurb.cloud "export CWD=$REMOTE_PRIORY_ROOT && cd $REMOTE_PRIORY_ROOT && $REMOTE_PRIORY_ROOT/create-users.sh"
+ssh -i $KEYFILE root@blurb.cloud "export CWD=$REMOTE_PRIORY_ROOT && cd $REMOTE_PRIORY_ROOT && $REMOTE_PRIORY_ROOT/install-toolchain.sh"
+ssh -i $KEYFILE root@blurb.cloud "export CWD=$REMOTE_PRIORY_ROOT && cd $REMOTE_PRIORY_ROOT && $REMOTE_PRIORY_ROOT/install-all-services.sh"
