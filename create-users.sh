f9d47680d637bc8f0003f55c8bb6e43f4ac571bb
Ted Singer <tedsinger1@gmail.com>
2022-06-05 21:26:05 -0400

    rename to onemanband

--- create-users.sh
+++ create-users.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-groupadd prior
+groupadd onemanband
 for u in `ls services/`; do
-    useradd $u -g prior -m
+    useradd $u -g onemanband -m
     for f in `ls -A userstuff/`; do
         cp userstuff/$f /home/$u/
         chsh $u -s /bin/bash
         chown $u /home/$u/$f
-        chgrp prior /home/$u/$f
+        chgrp onemanband /home/$u/$f
     done
 done
