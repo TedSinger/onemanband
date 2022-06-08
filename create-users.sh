75fd950e8a9110a34566be587b6b325d1b5991ee
Ted Singer <tedsinger1@gmail.com>
2022-06-08 17:23:40 -0400

    more shellcheck

--- create-users.sh
+++ create-users.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s dotglob
 groupadd onemanband
-for u in `ls services/`; do
-    useradd $u -g onemanband -m
-    for f in `ls -A userstuff/`; do
-        cp userstuff/$f /home/$u/
-        chsh $u -s /bin/bash
-        chown $u /home/$u/$f
-        chgrp onemanband /home/$u/$f
+for user_folder in services/*; do
+    u=$(basename "$user_folder")
+    useradd "$u" -g onemanband -m
+    for f in userstuff/*; do
+        f=$(basename "$f")
+        cp userstuff/"$f" /home/"$u"/
+        chsh "$u" -s /bin/bash
+        chown "$u" /home/"$u"/"$f"
+        chgrp onemanband /home/"$u"/"$f"
     done
 done
